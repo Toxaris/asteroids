46240b9e211e6252eace29aff7fe729881097129
Tillmann Rendel <rendel@informatik.uni-marburg.de>
2012-08-17 18:08:46 +0200

    Add file for game rules.

---
+++ src/asteroids/Rules.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package asteroids;
+
+/**
+ * Game rules.
+ * 
+ * @author Tillmann Rendel
+ */
+public abstract class Rules {
+	/**
+	 * Acceleration of the player's ship, in units per tick per tick.
+	 */
+	public static final double SHIP_ACCELERATION = 0.01;
+}
